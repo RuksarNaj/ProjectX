cc50afca29ee69d1f8a09dddd90a2afaa60c414f
Ruksar.N <ruksarnaj8@gmail.com.com>
2024-10-01 14:39:13 +0530

    Adding project files

---
+++ src/main/java/com/tecnics/SampleQube/App.java
@@ -0,0 +1,13 @@
+package com.tecnics.SampleQube;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
